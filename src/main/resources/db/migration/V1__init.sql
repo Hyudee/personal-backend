fea0a963398de279b9c463fc2b49a4701e627c7d
Davi Hyude <hyudedavi1@gmail.com>
2026-09-06 12:14:05 -0300

    Add v2 migration

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -101,7 +101,7 @@ CREATE TABLE plans (
                        id BIGSERIAL PRIMARY KEY,
                        name VARCHAR(255) NOT NULL,
                        price NUMERIC(10,2) NOT NULL CHECK (price >= 0),
-                       periodicity VARCHAR(20) NOT NULL CHECK (periodicity IN ('MONTHLY', 'QUARTERLY', 'YEARLY'))
+                       periodicity VARCHAR(20) NOT NULL CHECK (periodicity IN ('MONTHLY', 'YEARLY'))
 );
 
 CREATE TABLE billing_records (
